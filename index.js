9c2c962aa78b9586064b4947f5cf5fcadff5c59f
Diana C Peterson <dpeter10@gmail.com>
2022-09-20 15:14:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
+// const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+
+// function() {
+//     alert('I was clicked!');
+//   }
+    
 function addingEventListener() {
+
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+    }
+const sinon = require('sinon');
+
+describe("index.js", () => {
+  let input;
+
+  beforeEach(function() {
+    input = document.getElementById('button');
+    sinon.spy(input, 'addEventListener');
+  })
+
+  it("binds an event listener in addingEventListener()", () => {
+    addingEventListener();
+    expect(input.addEventListener.called).to.be.true;
+  })
+})
+ 
